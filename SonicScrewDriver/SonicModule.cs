c32a415c5c35b11dc9bad30a15a0983beeb10c31
Wyatt Putnam <putnamwt@dukes.jmu.edu>
2021-07-30 13:47:41 -0400

    Comments

--- SonicScrewDriver/SonicModule.cs
+++ SonicScrewDriver/SonicModule.cs
@@ -8,40 +8,34 @@ using UnityEngine;
 
 namespace SonicScrewDriver
 {
+    public class SonicModule : ItemModule
+    {
+        //2, 3, 4, 10, 11
+        public int sonicType;
 
-        public class SonicModule : ItemModule
+
+        //Sounds setup
+        public String sonic; 
+        public String extend; 
+        public String unClick; 
+        public String click; 
+        public String clasp; 
+        public String jingle; 
+
+        //Animation setup
+        public String sonicExtendAnimate;
+        public String sonicRetract;
+        public String extendHash;
+        public String retractHash;
+        public String extendAnimPathHash;
+        public String retractAnimPathHash;
+        public String slide;
+
+
+        public override void OnItemLoaded(Item item)
         {
-            //2, 3, 4, 10, 11
-            public int sonicType;
-
-
-            //Sounds setup
-            public String sonic; 
-            public String extend; 
-            public String unClick; 
-            public String click; 
-            public String clasp; 
-            public String jingle; 
-
-            //Animation setup
-            public String sonicExtendAnimate;
-            public String sonicRetract;
-            public String extendHash;
-            public String retractHash;
-            public String extendAnimPathHash;
-            public String retractAnimPathHash;
-            public String slide;
-
-
-            public override void OnItemLoaded(Item item)
-            {
-                base.OnItemLoaded(item);
-                item.gameObject.AddComponent<Sonic>();
-            }
-
-            
-        }
-
-
-
+            base.OnItemLoaded(item);
+            item.gameObject.AddComponent<Sonic>();
+        }        
+    }
 }
